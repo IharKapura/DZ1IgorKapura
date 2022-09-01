5f48877ce1ca22d7f494992a058a3613418801ec
IharKapura <110481409+IharKapura@users.noreply.github.com>
2022-09-01 11:49:07 +0300

    Add files via upload

--- dom1igorkapura.py
+++ dom1igorkapura.py
@@ -1,58 +1,105 @@
 
+
 import psutil
 
-# В связи с тем что пропустил несколько занятий надо было много нагонять.
-# И перед тем как выпонлить домашние задание надо было просмотреть много пропущенной информации.
-# Часть инфы стал гуглить, и один из методов что нашел и решил попробовать это словари.
-def _RAM():
-    print("RAM:")
-    ram_dict = {}
-    ram = psutil.virtual_memory()
-    ram_dict['Used'] = str(int(ram.used / (1024 ** 3))) + " GB"
-    ram_dict['Free'] = str(int(ram.available / (1024 ** 3))) + " GB"
-    ram_dict['Total'] = str(int(ram.total / (1024 ** 3))) + " GB"
-    
-    return ram_dict
 
+def info_RAM():
+    global ram_data
+    ram_data = {}
+    ram = psutil.virtual_memory()
+    ram_data.update(total_ram = ram.total,
+                    used_ram = ram.used,
+                    percent_ram = ram.percent,
+                    available_ram = ram.available,
+                    free_ram = ram.used)
+                    
+    return ram
 
 def info_disk():
-    print ("Disc Information:")
+    global disk
     disk = {}
     data_disk = psutil.disk_usage('/')
-    disk.update(total_disk = str(int(data_disk.total/(1024**3))) + " GB",
-                used_disk = str(int(data_disk.used / (1024**3))) + " GB",
-                free_disk = str(int(data_disk.free / (1024**3))) + " GB",   
-                useddisk_percent = str(data_disk.percent) + "%" )  
+    disk.update(total_disk = data_disk.total,
+                used_disk = data_disk.used,
+                free_disk = data_disk.free,   
+                useddisk_percent = data_disk.percent)  
                 
     return disk
 
 
 def info_battery():
-    print("Battery Information:")
+    global battery
     battery = {}
     data_baterry = psutil.sensors_battery()
-    battery.update(charge_percantage = str(data_baterry.percent) + "%",
+    battery.update(charge_percantage = data_baterry.percent,
                     on_charge = data_baterry.power_plugged)
     
     return battery                
 
 
-def CPU_frequency():
-    print("CPU frequency:")
+def info_CPU_frequency():
+    global CPU
     CPU = {}
     data_CPU = psutil.cpu_freq()
-    CPU.update(current_freq = str(data_CPU.current) + " GHz",
-                min_freq = str(data_CPU.min) + " GHz",
-                max_freq = str(data_CPU.max) + " GHz")
+    CPU.update(current_freq = data_CPU.current,
+                min_freq = data_CPU.min,
+                max_freq = data_CPU.max)
     
     return CPU
 
 
-def info_system():
-    print(CPU_frequency())
-    print(_RAM())
-    print(info_disk())
-    print(info_battery())
+def out_info():
+    info_disk()
+    info_battery()
+    info_RAM()
+    info_CPU_frequency()
 
 
-info_system()
+out_info()
+def change_info_all():
+    def change_info_disk():
+        global _DISK_C
+        _DISK_C = ("DISK C:",
+        "| Total: " + str(int((disk["total_disk"])/ (1024**3))) + "GB",
+        "| Used: " + str(int((disk["used_disk"])/ (1024**3))) + " GB",
+        "| Free: " + str(int((disk["free_disk"])/ (1024**3))) + " GB",
+        "| Percent: " + str(disk["useddisk_percent"]) + " %")
+    change_info_disk()
+
+
+    def change_info_CPU_frequency():
+        global _Data_CPU
+        _Data_CPU = ("Data CPU:",
+        "| Cerrent Frequency: " + str(CPU["current_freq"]) + " GHz",
+        "| MIN Frequency: " + str(CPU["min_freq"]) + " GHz",
+        "| MAX Frequency: " + str(CPU["max_freq"]) + " GHz")
+    change_info_CPU_frequency()
+
+    def change_info_ram():
+        global _RAM
+        _RAM = ("RAM:",
+        "| Total Memory: " + str(int((ram_data["total_ram"])/(1024**3))) + " GB",
+        "| Used Memory: " + str(int((ram_data["used_ram"])/(1024**3))) + " GB",
+        "| Percentage Employed: " + str(ram_data["percent_ram"]) + " %",
+        "| Available Memory: " + str(int((ram_data["available_ram"])/(1024**3))) +  " GB",
+        "| Free Memory: " + str(int((ram_data["free_ram"])/(1024**3))) + " GB")
+    change_info_ram()
+
+
+    def change_info_baterry():
+        global _Baterry
+        _Baterry = ("BATERRY: ",
+        "| Baterry:" + str(battery["charge_percantage"]) + " %",
+        "| ON Charge: " + str(battery["on_charge"]))
+    change_info_baterry()
+
+change_info_all()
+
+def print_info_all():
+    print(*_DISK_C, sep = '\n')
+    print(*_Data_CPU, sep = '\n')
+    print(*_RAM, sep = '\n')
+    print(*_Baterry, sep = '\n')
+
+
+print_info_all()
