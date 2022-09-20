f84a4d82eba2249fce466b4b7cf9e907fe286319
IharKapura <110481409+IharKapura@users.noreply.github.com>
2022-09-20 11:57:28 +0300

    Декаратор для записи данных в файлы JSON

--- dom1igorkapura.py
+++ dom1igorkapura.py
@@ -8,6 +8,14 @@ V_memory = {}
 CPU = {}
 
 
+def write_file(func_to_write):
+    def func_write(name_file, name_data):
+        with open(name_file, 'w') as file:
+            json.dump(name_data, file)
+        func_to_write(name_file, name_data)
+    return func_write
+
+
 def info_computer():
     ram = psutil.virtual_memory()
     ram_data.update(total_ram = ram.total,
@@ -33,44 +41,9 @@ def info_computer():
     CPU.update(current_freq = data_CPU.current,
                 min_freq = data_CPU.min,
                 max_freq = data_CPU.max)
-    
-    return disk, ram_data, CPU, V_memory
 
 
 info_computer()
-
-
-def change_info():
-    global disk_c, _cpu, _ram_d, _mem
-    disk_c = ("DISK C:",
-            "| Total: " + str(int((disk["total_disk"])/ (1024**3))) + " GB",
-            "| Used: " + str(int((disk["used_disk"])/ (1024**3))) + " GB",
-            "| Free: " + str(int((disk["free_disk"])/ (1024**3))) + " GB",
-            "| Percent: " + str(disk["useddisk_percent"]) + " %",)
-    
- 
-    _cpu = ("Data CPU:",
-            "| Cerrent Frequency: " + str(CPU["current_freq"]) + " GHz",
-            "| MIN Frequency: " + str(CPU["min_freq"]) + " GHz",
-            "| MAX Frequency: " + str(CPU["max_freq"]) + " GHz")
-
-
-    _ram_d = ("RAM:",
-            "| Total Memory: " + str(int((ram_data["total_ram"])/(1024**3))) + " GB",
-            "| Used Memory: " + str(int((ram_data["used_ram"])/(1024**3))) + " GB",
-            "| Percentage Employed: " + str(ram_data["percent_ram"]) + " %",
-            "| Available Memory: " + str(int((ram_data["available_ram"])/(1024**3))) +  " GB",
-            "| Free Memory: " + str(int((ram_data["free_ram"])/(1024**3))) + " GB")
-
-
-    _mem = ("VIRTUAL_MEMORY: ",
-            "| USED: " + str(int((V_memory["total"])/(1024**3))) + " GB",
-            "| FREE: " + str(int((V_memory["available"])/(1024**3))) + " GB")
-
-    return disk_c, _ram_d, _cpu, _mem
-
-
-change_info()
 
 
 def print_info():
@@ -98,22 +71,26 @@ def print_info():
     _mem = print("VIRTUAL_MEMORY: ",
             "| USED: " + str(int((V_memory["total"])/(1024**3))) + " GB",
             "| FREE: " + str(int((V_memory["available"])/(1024**3))) + " GB", sep = '\n')
-
-    
-    
+  
     return disk_c, _ram_d, _cpu, _mem
 
 
 print_info()
 
 
-def write_file():
-    to_file_json = [disk_c, _ram_d,_cpu,_mem]
+def write_file_all_info():
+    to_file_json = (disk, ram_data,CPU,V_memory)
 
-    with open('data_infoproba.json', 'w') as file:
+    with open('data_info_all.json', 'w') as file:
         json.dump(to_file_json, file)
 
-    return to_file_json
+write_file_all_info()  
 
 
-write_file()    
+@write_file
+def write_data_in_file(name_file, name_data):
+    print( 'All GOOD! data written to file:', name_file)
+write_data_in_file('ram_data.json',ram_data)
+write_data_in_file('disk.json',disk)
+write_data_in_file('virtual_memory.json',V_memory)
+write_data_in_file('CPU.json',CPU)
